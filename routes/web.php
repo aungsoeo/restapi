d65d46c7d0a38b0432bad7d15dd0b56fe65ca1c3
aungsoeoo <aungsoeoo.94s@gmail.com>
2019-11-26 17:43:27 +0630

    add auth in web

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,11 @@
 */
 
 Route::get('/', function () {
-    return redirect('products');
+    return view('welcome');
 });
 
 
 Route::resource('products','ProductController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
